a68e2523221836d6dc7e88c131d242de3ad78747
Ihor Vereshchynskyi <ex10dios@gmail.com>
2025-09-19 00:27:05 +0300

    1st and 3rd person character prefabs

---
+++ Assets/ExtendStudio/FinalCharacterConroller/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace ExtendStudio.FinalCharacterController
+{
+    [DefaultExecutionOrder(-1)]
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Camera _playerCamera;
+
+        [Header("Base Movement")]
+        public float runAcceleration = 1f;
+        public float runSpeed = 10f;
+        public float drag = 1f;
+
+        [Header("Camera Settings")]
+        public float lookSenseH = 1f;
+        public float lookSenseV = 1f;
+        public float lookLimitV = 89f;
+
+        private PlayerLocomotionInput _playerLocomotionInput;
+        private Vector2 _cameraRotation = Vector2.zero;
+        private Vector2 _playerTargetRotation = Vector2.zero;
+
+        private void Awake()
+        {
+            _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        }
+
+        private void Update()
+        {
+            Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
+            Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0f, _playerCamera.transform.right.z).normalized;
+
+            Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
+            Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
+
+            Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+            Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+
+            newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+            newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed);
+
+            // Move character (Unity suggests only calling this once per tick)
+            _characterController.Move(newVelocity * Time.deltaTime);
+        }
+
+        private void LateUpdate()
+        {
+            _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
+            _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV * _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
+
+            _playerTargetRotation.x += transform.eulerAngles.x + lookSenseH * _playerLocomotionInput.LookInput.x;
+
+            transform.rotation = Quaternion.Euler(0f, _playerTargetRotation.x, 0f);
+
+            _playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
+        }
+    }
+}
